56565079c446c53f50fbf0bb0f83fca4958b52e6
Jie-Ting Jiang <stu87282@gmail.com>
2019-01-17 12:31:16 +0800

    redo multiples.py

--- part1_multiples.py
+++ part1_multiples.py
@@ -5,21 +5,23 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
 def find_multiples_of_3_or_5(below):
-    ans = []
+    # ans = []
     
     # multiples of 3
-    n = 0
-    while 3*n < below:
-        ans.append(3*n)
-        n += 1
+    # n = 0
+    # while 3*n < below:
+    #    ans.append(3*n)
+    #    n += 1
     
     # multiples of 5
-    n = 0
-    while 5*n < below:
-        # if n is not a multiple of 3, 5*n won't be.
-        if n%3 != 0:
-            ans.append(5*n)
-        n += 1
-    return ans
+    # n = 0
+    # while 5*n < below:
+    #     # if n is not a multiple of 3, 5*n won't be.
+    #     if n%3 != 0:
+    #         ans.append(5*n)
+    #     n += 1
+    # return ans
+    mult = [i for i in range(below) if i % 3 == 0 or i % 5 == 0]
+    return mult
 
 print(sum(find_multiples_of_3_or_5(1000)))
